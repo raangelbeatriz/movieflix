cc9031bc52eef5d6f0e47d622c6d6fb7e2421443
Beatriz Rangel <raangelbeatriz@gmail.com>
2023-02-05 16:13:13 -0300

    delete size extensions

--- lib/app/core/ui/helpers/size_extensions.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-extension SizeExtensions on BuildContext {
-  double get width => MediaQuery.of(this).size.width;
-  double get heigth => MediaQuery.of(this).size.height;
-
-  double percentWidth(double percent) => width * percent;
-  double percentHeigth(double percent) => heigth * percent;
-}
